7a2f2ae4196707e70bf667c79155652e8987af30
Simon Ferquel <simon.ferquel@docker.com>
2016-10-04 15:59:24 +0200

    Added a io.Reader / io.Writer conformant type that handles correctly the fact that p9p can do partial read / writes with large values
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- api/go-datakit/client.go
+++ api/go-datakit/client.go
@@ -265,3 +265,50 @@ func (f *FileReader) Read(p []byte) (int, error) {
 	}
 	return n, err
 }
+
+type ioFileReaderWriter struct {
+	f      *File
+	ctx    context.Context
+	offset int64
+}
+
+// NewIOReader creates a standard io.Reader at a given position in the file
+func (f *File) NewIOReader(ctx context.Context, offset int64) io.Reader {
+	return &ioFileReaderWriter{f, ctx, offset}
+}
+
+// NewIOWriter creates a standard io.Writer at a given position in the file
+func (f *File) NewIOWriter(ctx context.Context, offset int64) io.Writer {
+	return &ioFileReaderWriter{f, ctx, offset}
+}
+
+func (r *ioFileReaderWriter) Read(p []byte) (n int, err error) {
+	r.f.m.Lock()
+	defer r.f.m.Unlock()
+	n, err = r.f.c.session.Read(r.ctx, r.f.fid, p, r.offset)
+	r.offset += int64(n)
+	// additional error handling for compliying with io.Reader
+	if n == 0 && (err == nil || err == io.EOF) {
+		err = io.ErrUnexpectedEOF
+	}
+	if n < len(p) && err == io.EOF {
+		err = io.ErrUnexpectedEOF
+	}
+	return
+}
+
+func (w *ioFileReaderWriter) Write(p []byte) (n int, err error) {
+	w.f.m.Lock()
+	defer w.f.m.Unlock()
+	for err == nil {
+		var written int
+		written, err = w.f.c.session.Write(w.ctx, w.f.fid, p, w.offset)
+		p = p[written:]
+		w.offset += int64(written)
+		n += written
+		if len(p) == 0 {
+			break
+		}
+	}
+	return
+}
